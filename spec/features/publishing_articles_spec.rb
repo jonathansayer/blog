86d4ef102e22b3b3f697bb0d2efe5fabab8d39f6
jonathansayer <jon.sayer1@gmail.com>
2015-12-13 11:31:11 +0000

    added feature tests to test that articles are not being deleted when there are unpublished

--- spec/features/publishing_articles_spec.rb
+++ spec/features/publishing_articles_spec.rb
@@ -16,4 +16,23 @@ context 'when publishing an article' do
     expect(page).to have_content "Test"
     expect(page).to have_content "This is an Article"
   end
+
+  scenario "should be able to remove the article" do
+    create_second_test_article
+    click_button "Test"
+    expect(current_path).to eq '/articles_show'
+    expect(page).to_not have_content "Test2"
+    click_button "remove_Test"
+    expect(current_path).to eq '/articles_show'
+    expect(page).to_not have_content "Test"
+  end
+
+  scenario 'articles that have been removed from the show page are not deleted' do
+    click_button "Test"
+    click_button "remove_Test"
+    articles = Article.all
+    published = Article.where(published?:true)
+    expect(published.count).to eq 0
+    expect(articles.count).to eq 1
+  end
 end
