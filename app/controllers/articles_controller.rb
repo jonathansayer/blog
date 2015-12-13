8ad05cf882b69f223be2aaaf4810aea311b05eb8
jonathansayer <jon.sayer1@gmail.com>
2015-12-13 10:24:14 +0000

    introduced not signed in error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    redirect_to root_path if !admin_signed_in?
+    if !admin_signed_in?
+      redirect_to root_path
+      flash[:error] = "You are not signed in as an administrator"
+    end
   end
 
   def save
--- spec/features/admin_features_spec.rb
+++ spec/features/admin_features_spec.rb
@@ -6,11 +6,13 @@ feature 'admin rights' do
     scenario "visiting create page redirects to index" do
       visit 'articles_create'
       expect(current_path).to eq '/'
+      expect(page).to have_content 'You are not signed in as an administrator'
     end
 
     scenario 'visiting publish page redirects to index' do
       visit 'articles_publish'
       expect(current_path).to eq '/'
+      expect(page).to have_content 'You are not signed in as an administrator'
     end
   end
 end
